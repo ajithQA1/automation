b1ffd1d3746a65f2a8398c9402f3bc86cc144b87
ajithQA1 <128983870+ajithQA1@users.noreply.github.com>
2023-03-26 20:33:22 +0530

    Add files via upload

---
+++ Loop3.java
@@ -0,0 +1,18 @@
+package org.loop2;
+
+public class Loop3 {
+public static void main(String[] args) {
+	// nested for loop 
+	for (int i = 1; i <=5; i++) {
+		for (int j = 1; j <=4; j++) {
+			
+	
+		System.out.print(j);
+		
+	}
+	System.out.println();
+		
+		
+	}
+}
+}
